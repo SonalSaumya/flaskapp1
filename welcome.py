4712786ba92591c392099fc7e3369368c071320d
dai <dai@students.act.in>
2024-12-26 15:29:26 +0530

    welcome.py and docker added

--- welcome.py
+++ welcome.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def welcome():
-	return"Welcome to Kubernets"
+	return "Welcome to Kubernets"
 
-if __name__='__main__':
+if __name__ ==  '__main__':
 	app.run(host='0.0.0.0', port='7000', debug=True)
