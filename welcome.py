adf8b701f05a5a12f43e3a6cb92dd5cccf3c35a8
dai <dai@students.act.in>
2024-12-28 13:21:40 +0530

    welcome.py mod4

--- welcome.py
+++ welcome.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def welcome():
-	return "The Jenkins CICD pipeline is working."
+	return "It's Lunch time..!!!"
 
 if __name__ ==  '__main__':
 	app.run(host='0.0.0.0', port='7000', debug=True)
